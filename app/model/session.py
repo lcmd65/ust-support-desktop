105cedabf56da3bdec6842989554ff68a99b8b0d
Le Chon Minh Dat <93373784+lcmd65@users.noreply.github.com>
2023-09-14 05:21:46 +0700

    Delete app directory

--- app/model/session.py
+++
@@ -1,5 +0,0 @@
-class UserRequest():
-    def __init__(self, id, question, answer):
-        self.id = id
-        self.question = [].append(question)
-        self.answer = [].append(answer)
--- app/view/var.py
+++
@@ -1,3 +0,0 @@
-background_view = None
-background_view_toplevel = None
-logo_view = None
