047147f8a7036535c57f06a333f706c8d21442cf
Dat <dat.lemindast@gmail.com>
2023-09-17 13:33:01 +0700

    change

--- test/test.py
+++ test/test.py
@@ -1,30 +1,11 @@
 
-import transformers
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-token_ = "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"
-token_write = "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"
-tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-180B", token = token_)
-model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-180B", token = token_)
-
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device_map="auto",
-)
-
-sequences = pipeline(
-   "Xin chào, cho tôi xin thông tin chương trình đào tạo của hcmus",
-    max_length=200,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-)
-
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+def questionAnswering(question_, context_):
+    from transformers import pipeline, AutoModelForQuestionAnswering, AutoTokenizer
+    # Load the question answering model and tokenizer
+    model = AutoModelForQuestionAnswering.from_pretrained("ancs21/xlm-roberta-large-vi-qa")
+    tokenizer = AutoTokenizer.from_pretrained("ancs21/xlm-roberta-large-vi-qa")
+    # Create a question answering pipeline
+    qa_pipeline = pipeline("question-answering", model=model, tokenizer=tokenizer)
+    # Get the answer
+    answer = qa_pipeline(question=question_, context=context_)
+        return answer
